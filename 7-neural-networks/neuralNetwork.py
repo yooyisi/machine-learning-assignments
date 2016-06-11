4618a7d965badd0e3a9dd3de0b042fd0fea7825f
Zhang Qi <kksjtu@gmail.com>
2016-06-11 17:20:35 +0200

    first try, feels wrong...

---
+++ 7-neural-networks/neuralNetwork.py
@@ -0,0 +1,72 @@
+import numpy as np
+import math
+from scipy.stats import logistic
+
+# hidden layer size
+hl_size = 100
+input_size = 3
+data = np.loadtxt("data2Class_adjusted.txt")
+inputs = data[:, :input_size]
+outputs = data[:, input_size]
+parameter_ini_1 = math.sqrt(3/input_size)
+parameter_ini_2 = math.sqrt(3/hl_size)
+w1 = np.random.random((hl_size,input_size))
+w2 = np.random.random((1, hl_size))
+w = [w1, w2]
+alpha = 0.05
+
+def forward(x, w):
+    """
+    execute for each input line
+    :param x: the inputs for each line
+    :param w:
+    :return:
+        x_new:  updated input for each layer
+        z:      the result in the end, and the new x
+    """
+    xi = x
+    x_layers = [x]
+    for wi in w:
+        #print "xi shape: " + str(xi.shape)
+        #print "wi shape: " + str(wi.shape)
+        z = np.dot(wi, xi)
+        xi = logistic.cdf(z)
+        x_layers.append(xi)
+    return x_layers, z
+
+
+def backward(delta_L_plus_1, x, w):
+    """
+    :param delta_L_plus_1:
+    :param x:   initial inputs from file
+    :param w:
+    :return:
+    """
+    xi, f = forward(x, w)
+    delta_p1 = delta_L_plus_1
+    dw_list =[]
+    for xb, wb in zip(reversed(xi[:-1]), reversed(w)):
+        delta = np.multiply(np.dot(delta_p1, wb), np.multiply(xb, 1-xb).T)
+        dw = np.dot(delta, xb.T)
+        delta_p1 = delta
+        dw_list.insert(0, dw)
+    return dw_list
+
+
+def hinge_loss(f, y):
+    return max(1-f*y, 0)
+
+
+def delta_l_plus_1(y, f):
+    if (1-f*y)>0:
+        return -1*y
+    else:
+        return 0
+
+
+for input, output in zip(inputs, outputs):
+    x_list, f = forward(input, w)
+    del_p1 = delta_l_plus_1(output, f)
+    dw_list = backward(del_p1, input, w)
+    w = tuple(wi - alpha * dwi for wi, dwi in zip(w, dw_list))
+    print hinge_loss(output, f)
